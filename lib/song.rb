d6876d0de9fced152335f7ec21808d68b90cc698
layla dantas <laylatdantas@gmail.com>
2019-06-17 01:28:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,15 @@
 class Song
-def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  end
-  attr_accessor :name, :artist, :genre
-end 
-
-
-
-
-
-
-
-#class Song
-   #attr_accessor :name, :artist,:genre
-   #def initialize(name, artist, genre)
-    # @name = name 
-     #@artist = artist
-    # @genre = genre 
+   attr_accessor :name, :artist,:genre
+   def initialize(name, artist, genre)
+    @name = name 
+     @artist = artist
+    @genre = genre 
     
- #end 
+ end 
  
- 
+ def self.count 
+    @@count
+  end
  
  
  
