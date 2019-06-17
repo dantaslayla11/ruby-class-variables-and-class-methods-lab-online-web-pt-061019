7c120639aaa3ad8785a9b6f7de7b673fbce1fcf5
layla dantas <laylatdantas@gmail.com>
2019-06-17 01:48:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ class Song
   
   @@count = 0
   @@artist = []
+  @@genre
   attr_accessor :name, :artist,:genre
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @@artist << artist
-    @genre = genre 
+    @genre = genre
+    @@genre << genre
     @@count = count + 1;
   end 
 
@@ -17,6 +19,10 @@ class Song
    
    def self.artist
      @@artist
+   end
+   
+   def self.genre
+     @@genre
    end
  
 end 
