89a6117b4b088747739ef2670656085a08d61135
layla dantas <laylatdantas@gmail.com>
2019-06-17 01:36:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,14 +5,10 @@ class Song
     @name = name 
      @artist = artist
     @genre = genre 
-    
+     @@count += 1
  end 
  
-   @@count = 0
-  
-  def self.count 
-    @@count
-  end 
+ 
   
  
  
